639255fd88074bebe849489f0503b9c72898ac7d
Wallace, Paul <paul.wallace@hyptherm.com>
2015-07-22 16:27:02 -0400

    Initial Commit

---
+++ src/serialportsniffer/SerialPortSniffer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialportsniffer;
+
+import java.io.*;
+import javax.comm.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author prwallace
+ */
+public class SerialPortSniffer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
